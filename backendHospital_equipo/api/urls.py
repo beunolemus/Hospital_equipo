f7117c33ab2e34b85c0007144331e49d30272f9c
Bruno Lemus <brno.lems@gmail.com>
2024-03-26 12:20:23 -0600

    Actualizacion del proyecto backendhospital_equipo

---
+++ backendHospital_equipo/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path,include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'c_cliente', views.c_clienteViewSet)
+router.register(r'c_rol', views.c_rolViewSet)
+router.register(r'CalendarioCirugia', views.CalendarioCirugiaViewSet)
+
+urlpatterns = [
+	path('api/v1',include(router.urls))
+]
+
